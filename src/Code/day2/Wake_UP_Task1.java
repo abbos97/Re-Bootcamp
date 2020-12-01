2e59869d4f3deb80e1a5c82ac4ff36cb3fd58bae
abbos97 <abbosboev@gmail.com>
2020-11-30 22:24:31 -0500

    Day 2. Wake up String reverse and Concating using

--- src/Code/day2/Wake_UP_Task1.java
+++ src/Code/day2/Wake_UP_Task1.java
@@ -24,8 +24,13 @@ public class Wake_UP_Task1 {
 
        // word2=word1.charAt(3)+""+word1.charAt(2)+""+word1.charAt(1)+""+word1.charAt(0);
         //word2=word2+word1.charAt(3)+word1.charAt(2)+word1.charAt(1)+word1.charAt(0);
-        word2+=word1.charAt(3)+word1.charAt(2)+word1.charAt(1)+word1.charAt(0);
+        word2+=word1.charAt(3)+""+word1.charAt(2)+word1.charAt(1)+word1.charAt(0);
 
         System.out.println("word2 = " + word2);
+
+        String msg="Hello ";
+        //msg = msg + "World!" ;
+        msg+="World!";
+        System.out.println("msg = " + msg);
     }
 }
